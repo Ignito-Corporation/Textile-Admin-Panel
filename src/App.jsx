d97b80911a724aae36d93296cfe578e7e00d7e44
Nishant Sharma <nishant20046@gmail.com>
2025-07-28 16:16:56 +0530

    Out Entery page of Knitting and dying

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AllPOs from "./pages/TotalPos";
 import BillEntry from "./pages/BillEnteryPurchase";
 import CreatePO from "./pages/CreatePO"
 import CreateBillEntry from "./pages/CreateBillEntry";
+import Out_Entery from "./pages/Out_Entery";
 
 const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(
@@ -42,7 +43,7 @@ const AppWrapper = () => {
         <Route index element={<Dashboard />} />
         <Route path="all-pos" element={<AllPOs />} />
         <Route path="bill-entry" element={<BillEntry />} />
-
+        <Route path='out-entry' element={<Out_Entery/>}/>
       </Route>
 
       <Route
